fd4755907447bd4d787f8ee1c247e8e19aa7dc2a
MORKOVKA25 <65350351+MORKOVKA25@users.noreply.github.com>
2022-04-21 01:12:25 +0300

    Add files via upload

--- myconfig.sh
+++ myconfig.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 sudo apt update
+sudo dpkg --configure -a
 sudo apt upgrade -y
-sudo apt install nano dev-tools vsftpd make -y
+sudo apt install lib32z1 lib32stdc++6 lib32gcc1 screen glances zip unzip htop neofetch python python-pip nano dev-tools vsftpd gcc make sl openjdk-16-jdk-headless openjdk-8-jdk-headless cmake -y
 sudo echo write_enable=YES >> /etc/vsftpd.conf
-sudo reboot
+echo neofetch >> .bashrc
+echo PS1="\e[1;38;2;0;200;100m\u@\h\e[m:\e[1;38;2;5;222;250m\w\e[m$ " >> .bashrc
+echo neofetch >> .zshrc
+echo PS1="\e[1;38;2;0;200;100m\u@\h\e[m:\e[1;38;2;5;222;250m\w\e[m$ " >> .zshrc
+sudo service vsftpd restart
