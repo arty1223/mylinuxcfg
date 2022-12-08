72829e99bff652d161f06208f316b61f76fcd1e3
MORKOVKA25 <65350351+MORKOVKA25@users.noreply.github.com>
2022-12-08 21:17:12 +0300

    Update myconfig.sh

--- myconfig.sh
+++ myconfig.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install lib32z1 lib32stdc++6 lib32gcc1 screen glances zip unzip htop neofetch python3 python-pip -y
+sudo apt install lib32z1 lib32stdc++6 lib32gcc1 screen glances zip unzip htop neofetch python3 python-pip git -y
 sudo apt install nano dev-tools vsftpd gcc make sl openjdk-16-jdk-headless openjdk-8-jdk-headless cmake python3-dev -y
 sudo echo write_enable=YES >> /etc/vsftpd.conf
 if test -f ~/.bashrc; then
