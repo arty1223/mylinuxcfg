f239427642885e13b672127dc7e6da461169deec
arty1223 <65350351+arty1223@users.noreply.github.com>
2024-09-02 01:47:19 +0300

    Update myconfig.sh
    openjdk-16-jdk-headless -> openjdk-21-jdk-headless

--- myconfig.sh
+++ myconfig.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install lib32z1 lib32stdc++6 lib32gcc1 screen glances zip unzip htop neofetch python3 python-pip git -y
-sudo apt install nano dev-tools vsftpd gcc make sl openjdk-16-jdk-headless openjdk-8-jdk-headless cmake python3-dev -y
+sudo apt install nano dev-tools vsftpd gcc make sl openjdk-21-jdk-headless openjdk-8-jdk-headless cmake python3-dev -y
 sudo echo write_enable=YES >> /etc/vsftpd.conf
 if test -f ~/.bashrc; then
     echo neofetch >> ~/.bashrc
